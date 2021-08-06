fef579adda79a24c5a7d111f9983e1a9078d50eb
Lucas <scazzariello.lucas@gmail.com>
2021-08-06 00:24:45 -0300

    maquetado searchBar

---
+++ components/SearchBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {Navbar, Input, Button, Container} from "reactstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+const SearchBar = () => {
+
+    return (
+        <Navbar className="nav-bar">
+            <Container className="nav-bar__container">
+                <a className="nav-bar__image"></a>
+                <div className="search-bar">
+                    <Input type="text" placeholder="Nunca dejes de buscar" className="search-bar__input"/>
+                    <Button className="btn btn-light search-bar__button">
+                        <span>
+                            <FontAwesomeIcon icon="search"/>
+                        </span>
+                    </Button>
+                </div>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default SearchBar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../assets/styles/main.scss'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
-function MyApp({ Component, pageProps }) {
+function App({ Component, pageProps }) {
+  library.add(faSearch)
+  // eslint-disable-next-line react/react-in-jsx-scope
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
